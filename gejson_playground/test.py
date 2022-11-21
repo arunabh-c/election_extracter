64dd86f76c285c47187ea1ad94b31b5cc4ac49c3
arun <arunabh71@gmail.com>
2022-11-20 22:23:38 -0800

    added time measuring code.

--- gejson_playground/test.py
+++ gejson_playground/test.py
@@ -1,8 +1,12 @@
 import numpy as np
-from shapely.geometry import Point
+from shapely.geometry import Point, shape
 from geopandas import read_file
 from xml.etree import ElementTree
+import shapely.speedups
+import keytree
+import time
 
+shapely.speedups.enable()
 federal_geojson = read_file("districts.geojson")
 federal_kml = open("cb_2018_us_cd116_500k.kml").read()
 state_house_geojson = read_file("CA/HOUSE/Legislative_Districts_in_California.geojson")
@@ -22,16 +26,31 @@ def get_district_kml(point: Point, file: str) -> str:
      # Filter polygon elements using this lambda (anonymous function)
      # keytree.geometry() makes a GeoJSON-like geometry object from an
      # element and shape() makes a Shapely object of that.
+     #cds = tree.findall(".//{%s}Polygon" % kmlns)
+     #for item in elems:
+         #print(item.Polygon)
+         #if item.contains(point):
+           #print(item)
      hits = filter(lambda e: shape(keytree.geometry(e)).contains(point),elems )
+     #print(file)
      #index = np.where(keytree.geometry(e)).contains(point)[0]
-     return hits
+     #return list(hits)[0]
+     print(len(elems))
+     print(elems[elems.index(list(hits)[0])-1])
+     return elems.index(list(hits)[0])
+     
  
 def find_district(lat, long):
         point = Point(float(lat), float(long))
         print(get_district_kml(point,federal_kml))
-        print(get_district_json(point,federal_geojson))
-        print(get_district_json(point,state_house_geojson))
-        print(get_district_json(point,state_senate_geojson))
-        print(get_district_json(point,state_school_geojson))
+        # print(get_district_json(point,federal_geojson))
+        # print(get_district_json(point,state_house_geojson))
+        # print(get_district_json(point,state_senate_geojson))
+        # print(get_district_json(point,state_school_geojson))
+
+start = time.time()
  
 find_district(-122.171520,37.448740)
+
+end = time.time()
+print("Time taken in seconds: " + str(end - start))
